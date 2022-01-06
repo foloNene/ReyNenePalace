5244f3c211a26ff1fe104a0716a6b52e8eaf942f
Olaide Adebanjo <olaideadebanjo@gmail.com>
2022-01-06 00:15:16 -0800

    Added PieApi contoller

---
+++ ReyNenePalace/Contollers/API/PieApiController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ReyNenePalace.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ReyNenePalace.Contollers.API
+{
+    [Route("api/pie")]
+    public class PieApiController: ControllerBase
+    {
+        private readonly IPieRepository _pieRepository;
+       
+        public PieApiController(IPieRepository pieRepository)
+        {
+            _pieRepository = pieRepository; 
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Pie>> GetPies()
+        {
+            return Ok(_pieRepository.AllPies);
+        }
+    }
+}
